ff7be6a7d4b6649b314c432be3b8cb37ca86ecb4
gsilvapt <gustavosantaremsilva@gmail.com>
2018-02-06 18:09:11 +0000

    Retries adding mongodb configuration.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,15 +1,8 @@
 var mongoose = require('mongoose');
 
-const REMOTE_MONGO = 'mongodb://<dbuser>:<dbpassword>@ds125288.mlab.com:25288/todo-api-sandbox';
-const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
-const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
-
 mongoose.Promise = global.Promise; // Sets which Promise library to use
-mongoose.connect(MONGO_URI).then(() => {
-  console.log('Connected to Mongo Instance.');
-}, (err) => {
-  console.log('Error connecting to Mongo instance:', err);
-});
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost.27017/TodoApp');
+
 
 module.exports = {
   mongoose
