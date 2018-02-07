f4fd917f7a1a79e2a2f97e2ce23e680d6750b4b9
gsilvapt <gustavosantaremsilva@gmail.com>
2018-02-07 13:54:45 +0000

    Rearranges project to work with Heroku.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // Sets which Promise library to use
-mongoose.connect(process.env.MONGOLAB_AMBER_URI || 'mongodb://localhost.27017/TodoApp');
+mongoose.connect(process.env.MONGOLAB_AMBER_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
