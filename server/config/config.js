13a92a118130a500426cfda249074a3ce4967c41
gsilvapt <gustavosantaremsilva@gmail.com>
2018-02-11 19:54:30 +0000

    Allows test suite configuration and live production one.

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,14 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development' || env === 'test') {
+if (env === 'development') {
   var config = require("./config.json");
   var envConfig = config[env];
   
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key]
   });
+} else if (env === 'test') {
+  process.env.PORT = 3000;	
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+  process.env.JWT_SECRET = 'megaSecretKey';
 }
